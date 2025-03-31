7315ca49417093b73dc97be97fd2808a64d075dd
Divya22-git <divyapandian2209@gmail.com>
2025-03-31 21:37:07 +0530

    Add files via upload

--- STUDENTMANAGEMENT.sql
+++ STUDENTMANAGEMENT.sql
@@ -48,3 +48,55 @@ UPDATE Students
 SET Grade = 'A' 
 WHERE StudentID = 5;
 SELECT * FROM Students WHERE StudentID = 5;
+USE studentmanagement;
+SHOW TABLES;
+DESC Students;
+DESC Courses;
+DESC Enrolments;
+CREATE TABLE Enrolments (
+	enrolment_id INT PRIMARY KEY,
+    StudentID INT,
+    course_id INT,
+    enrolment_date DATE,
+    FOREIGN KEY(StudentID) REFERENCES Students(StudentID),
+    FOREIGN KEY(course_id) REFERENCES Courses(course_id)
+);
+DESC Enrolments;
+INSERT INTO Courses (course_name, course_description) VALUES
+('Database Management', 'Learn about SQL databases.'),
+('Machine Learning', 'Introduction to ML concepts.'),
+('Web Development', 'Build dynamic websites.');
+INSERT INTO Enrolments (enrolment_id,StudentID, course_id, enrolment_date) VALUES
+(100,1, 1, '2025-03-01'), 
+(102,1, 2, '2025-03-02'), 
+(103,2, 1, '2025-03-03'), 
+(104,3, 3, '2025-03-04'); 
+SELECT * FROM Students;
+SELECT * FROM Courses;
+SELECT * FROM Enrolments;
+SELECT Students.name AS student_name, Courses.course_name
+FROM Students 
+INNER JOIN Enrolments ON Students.StudentID = Enrolments.StudentID
+INNER JOIN Courses ON Enrolments.course_id = Courses.course_id;
+SELECT Courses.course_name, COUNT(Enrolments.StudentID) AS student_count
+FROM Courses 
+LEFT JOIN Enrolments ON Courses.course_id = Enrolments.course_id
+GROUP BY Courses.course_id, Courses.course_name;
+SELECT Students.name AS student_name, COUNT(Enrolments.course_id) AS courses_enrolled
+FROM Students 
+JOIN Enrolments ON Students.StudentID = Enrolments.StudentID
+GROUP BY Students.StudentID, Students.name
+HAVING COUNT(Enrolments.course_id) > 1;
+SELECT Courses.course_name
+FROM Courses 
+LEFT JOIN Enrolments ON Courses.course_id = Enrolments.course_id
+WHERE Enrolments.enrolment_id IS NULL;
+
+
+
+
+
+
+
+
+
